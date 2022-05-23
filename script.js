3bff2afb1c695b6295ddd76295a981889c9db7a7
timur <email@email>
2022-05-23 21:25:33 +0500

    полурабочая версия

---
+++ script.js
@@ -0,0 +1,13 @@
+const users = {
+
+}
+
+for (let i = 1; i < 4; i++){
+    let name = prompt('Enter name')
+    let age = prompt('Enter your age')
+
+    users.name = name;
+    users.age = age
+
+    console.log(users);
+}
